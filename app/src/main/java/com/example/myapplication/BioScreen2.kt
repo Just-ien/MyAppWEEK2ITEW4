2712ba24267d742bb85c9051c40faed32c903c19
Student <irahchristinebrutas07@gmail.com>
2026-01-15 15:00:37 +0800

    BioScreen 2

---
+++ app/src/main/java/com/example/myapplication/BioScreen2.kt
@@ -0,0 +1,20 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BioScreen3() {
+
+    val name = "Irah Christine B. Brutas"
+    val section = "3IT-A"
+    val age = "21"
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Name: $name")
+        Text(text = "Section: $section")
+        Text(text = "Age: $age")
+    }
+}
